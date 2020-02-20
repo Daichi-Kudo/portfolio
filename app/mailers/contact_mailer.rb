7626e61b0cdf9d629f2994e37a1952f40116dd1e
dk <a_drowning_apple@yahoo.co.jp>
2020-02-20 13:08:48 +0000

    fix mailer

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,8 +1,7 @@
 class ContactMailer < ApplicationMailer
 
-  def contact_mail
-    @greeting = "Hi"
-
+  def contact_mail(contact)
+    @contact = contact
     mail to: ENV['MAIL'], subject: "ポートフォリオから"
   end
 end
